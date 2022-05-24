7fce274ecce823ac01d20211b20412726275579e
Hồ Đăng Dũng <dungho2501@gmail.com>
2022-05-24 20:39:20 +0700

    dũng

---
+++ MVC/Lab01/Lab01/Controllers/HelloWorldController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace Lab01.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWord/
+        //public String Index()
+        //{
+        //    return "This is my <b> default </b> action...<br>Hồ Đăng Dũng";
+        //}
+
+        //// GET: /HelloWord/Wellcome
+        //public String Wellcome()
+        //{
+        //    return "this is the wellcome to action method...<br>Hồ Đăng Dũng";
+        //}
+
+        // GET: /HelloWord/Wellcome?name=HoDangDung&numtimes=2389
+        //public String Wellcome(String name, int numtimes)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello " + name + " numtimes " + numtimes);
+        //}
+
+        // GET: /HelloWord/Wellcome?name=HoDangDung
+        /*public String Wellcome(String name, int id = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + id);
+        }*/
+
+        // GET: /HelloWord/Wellcome/3?name=HoDangDung
+        //public string Wellcome(String name, int id)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + id);
+        //}
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC/Lab01/Lab01/Controllers/TheloaitinController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab01.Models;
+
+namespace Lab01.Controllers
+{
+    public class TheloaitinController : Controller
+    {
+        // GET: Theloaitin
+        DataClasses1DataContext data = new DataClasses1DataContext();
+        public ActionResult Index()
+        {
+            var All_Loaitin = from tt in data.Theloaitins select tt;
+            return View();
+        }
+    }
+}
