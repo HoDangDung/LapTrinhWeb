ba14899c67c143cb628b765dc7481122b09e25fb
Hồ Đăng Dũng <dungho2501@gmail.com>
2022-06-16 13:15:24 +0700

    update

---
+++ LapTrinhWeb_TH/Buổi 5/HoDangDung/HoDangDung/Controllers/RubikController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HoDangDung.Models;
+
+namespace HoDangDung.Controllers
+{
+    public class RubikController : Controller
+    {
+        // GET: Rubik
+        dbRubikStoreDataContext data = new dbRubikStoreDataContext();
+        public ActionResult Index()
+        {
+            var all_rubik = from s in data.Rubiks select s;
+            return View(all_rubik);
+        }
+    }
+}
